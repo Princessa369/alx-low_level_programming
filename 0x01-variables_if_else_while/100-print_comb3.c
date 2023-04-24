39beee99b68f6f4e8cf17e8106c3595be1af6719
Princessa369 <princessprincewillobene@gmail.com>
2023-04-23 21:08:06 -0700

    task10 print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,15 +10,14 @@ int main(void)
 
 	for (a = 0; a <= 8; a++)
 	{
-		for (b = 0; b <= 9; b++)
-		{
-			putchar(a + '0');
-			putchar(b + '0');
-			putchar(',');
-			putchar(' ');
-		}
+	for (b = a + 1; b <= 9; b++)
+	{
+		putchar(a + '0');
+		putchar(b + '0');
+		putchar(',');
+		putchar(' ');
+	}
 	}
 	putchar('\n');
-
 	return (0);
 }
