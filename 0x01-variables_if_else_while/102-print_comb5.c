84159ba1327a5bff7de6c3a8bba7f33d2926ce9f
Princessa369 <princessprincewillobene@gmail.com>
2023-04-24 07:01:11 -0700

    task12 two two digits comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two two digit numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int c, d;
+
+	for (c = 0; c <= 99; c++)
+	{
+	for (d = c; d <= 99; d++)
+	{
+		putchar(c / 10 + '0');
+		putchar(c % 10 + '0');
+		putchar(' ');
+		putchar(d / 10 + '0');
+		putchar(d % 10 + '0');
+	if (c != 99 || d != 99)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
