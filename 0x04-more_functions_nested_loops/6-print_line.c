38d4037d0ffdc4e26122166f4d66a2a2b7f72abb
Princessa369 <princessprincewillobene@gmail.com>
2023-04-27 15:40:49 -0700

    task6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,12 +10,11 @@ void print_line(int n)
 {
 	if (n <= 0)
 	{
-		_putchar('\n');
-		return;
+	_putchar('\n');
 	}
-	for (int i = 0, i < n; i++)
-	{
-		_putchar('_');
-		_putchar('\n');
-	}
+for (int i = 0; i < n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
 }
