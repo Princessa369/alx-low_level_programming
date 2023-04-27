2e5feb228df2b6d957458320905b42f75a0c6ba2
Princessa369 <princessprincewillobene@gmail.com>
2023-04-27 09:55:38 -0700

    task2 mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first number
+ * @b: second number
+ * Return: multiplication
+ */
+
+int mul(int a, int b)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
