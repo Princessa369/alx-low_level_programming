26e31ab7d1d525a17af2224935f824ff0d98e70a
Princessa369 <princessprincewillobene@gmail.com>
2023-04-28 01:13:27 -0700

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,15 +10,21 @@ void print_diagonal(int n)
 	if (n <= 0)
 	{
 		_putchar('\n');
-		return;
 	}
-	for (int i = '0'; i < n; i++)
+	else
 	{
-	for (int j = '0'; j < i; j++)
-	{
-		_putchar(' ');
-	}
-	_putchar('\\');
-	_putchar('\n');
-	}
+	int i, j;
+
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < n; j++)
+{
+if (j == i)
+_putchar('\\');
+else if (j < i)
+_putchar(' ');
+}
+_putchar('\n');
+}
+}
 }
