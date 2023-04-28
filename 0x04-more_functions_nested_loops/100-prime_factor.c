da672b7eff3036a476964c2ead283e3eda9e8c41
Princessa369 <princessprincewillobene@gmail.com>
2023-04-28 14:34:49 -0700

    task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,27 +8,16 @@
 
 int main(void)
 {
-	long int n;
-	long int max;
-	long int i;
+	int a;
+	long num = 612852475143;
 
-n = 612852475143;
-max = -1;
-while (n % 2 == 0)
-{
-max = 2;
-n /= 2;
-}
-for (i = 3; i <= sqrt(n); i = i + 2)
-{
-while (n % i == 0)
-{
-max = i;
-n = n / i;
-}
-}
-if (n > 2)
-max = n;
-printf("%ld\n", max);
-return (0);
+	for (a = (int) sqrt(num); a > 2; a++)
+	{
+	if (num % a == 0)
+	{
+	printf("%d\n", a);
+	break;
+	}
+	}
+	return (0);
 }
