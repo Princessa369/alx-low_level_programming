8c63e1e820a64ac2c19281653401d28dac353428
Princessa369 <princessprincewillobene@gmail.com>
2023-05-08 11:40:56 -0700

    task 12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -9,35 +9,40 @@
 
 void print_buffer(char *b, int size)
 {
-	int a, d, c;
+	int o, j, i;
 
+	o = 0;
 	if (size <= 0)
-		printf("\n");
-	else
 	{
-	for (a = 0; a < size; a += 10)
-	{
-		printf("%.8x:", a);
-	for (d = a; d < a + 10; d++)
-	{
-	if (d % 2 == 0)
-		printf(" ");
-	if (d < size)
-		printf("%.2x", *(b + d));
-	else
-		printf(" ");
+	printf("\n");
+	return;
 	}
-	printf(" ");
-	for (c = a; c < a + 10; c++)
+	while (o < size)
 	{
-	if (c >= size)
-		break;
-	if (*(b + c) < 32 || *(b + c) > 126)
-		printf("%c", '.');
+	j = size - o < 10 ? size - o : 10;
+	printf("%.8x:", o);
+	for (i = 0; i < 10; i++)
+	{
+	if (i < j)
+		printf("%02x", *(b + o + i));
 	else
-		printf("%c", *(b + c));
+		printf(" ");
+	if (i % 2)
+	{
+	printf(" ");
+	}
+	}
+	for (i = 0; i < j; i++)
+	{
+	int c = *(b + o + i);
+
+	if (c < 32 || c > 132)
+	{
+	c = '.';
+	}
+	printf("%c", c);
 	}
 	printf("\n");
-	}
+	o += 10;
 	}
 }
