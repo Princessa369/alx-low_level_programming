1f6379ae7c6539fbfb5fc94194bbc661fa02f59f
Princessa369 <princessprincewillobene@gmail.com>
2023-05-12 08:43:22 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the name of program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0
+ */
+
+int main(int argc__attribute__((unused)), char *argv[])
+{
+	peintf("%s\n", *argv);
+	return (0);
+}
