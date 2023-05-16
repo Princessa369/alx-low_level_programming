d4658f3ecce607deb1a33a0f3179c9edd3a16181
Princessa369 <princessprincewillobene@gmail.com>
2023-05-16 02:52:28 -0700

    task 1, malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Returns a pointer to a newly allocated space in memory
+ * @str: string of characters
+ * Return: 0
+ */
+
+char *_strdup(char *str)
+{
+	char *new;
+	int i, j;
+
+	if (str == NULL)
+		return (NULL);
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+
+	new = malloc(sizeof(char) * (i + 1));
+	
+	if (new == NULL)
+	return (NULL);
+
+	for (j = 0; str[j]; j++)
+	new[j] = str[j];
+	return (new);
+	}
